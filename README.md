f65a876c49c325fc16985f241ecbfce35be69105
Adrian Edy <adrianedypratama@gmail.com>
2021-10-24 23:23:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Vue Coach App
+
+Simple [Vue](https://vuejs.org/) app focusing on implementing [Vue Composition API](https://v3.vuejs.org/guide/composition-api-introduction.html).
+
+## Installation & Usage
+
+Use your favorite Javascript package manager to install the dependencies, code below is using [npm](https://www.npmjs.com/).
+
+```bash
+npm install
+```
+
+Run the app with:
+
+```bash
+npm run serve
+```
+
+Or build the app to dist folder:
+
+```bash
+npm run build
+```
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/dummy-data.js
@@ -0,0 +1,33 @@
+export default [
+  {
+    id: 'lorem',
+    fullName: 'Lorem Ipsum',
+    projects: [
+      { id: 'lorem_p1', title: 'Record the Vue course' },
+      { id: 'lorem_p2', title: 'Create more courses' },
+      { id: 'lorem_p3', title: 'Keep content updated' }
+    ]
+  },
+  {
+    id: 'john',
+    fullName: 'John Doe',
+    projects: [
+      { id: 'john_p1', title: 'Create more courses' },
+      { id: 'john_p2', title: 'Dive into data science topics' },
+      { id: 'john_p3', title: 'Various things' }
+    ]
+  },
+  {
+    id: 'jane',
+    fullName: 'Jane Doe',
+    projects: [
+      { id: 'jane_p1', title: 'Create agenda for next months' },
+      { id: 'jane_p2', title: 'Explore new topics' }
+    ]
+  },
+  {
+    id: 'roe',
+    fullName: 'John Roe',
+    projects: []
+  }
+];
